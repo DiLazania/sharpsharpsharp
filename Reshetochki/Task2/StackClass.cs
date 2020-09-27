96b053c7f4d562d8771b1780a903b65a2d5c8377
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-09-28 01:55:35 +0300

    Task_2 : Implemented basic stack class.

---
+++ Reshetochki/Task2/StackClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    public class StackElement<T>
+    {
+        public StackElement<T> _nextElement;
+        public T data { get; }
+        public StackElement(T data) => this.data = data;
+    }
+
+    public class StackT<T>
+    {
+        private StackElement<T> top;
+
+        public int ElemCount { get; private set; }
+        public bool IsEmpty()
+        {
+            if (ElemCount == 0) return true;
+            return false;
+        }
+        public void Push(T obj)
+        {
+            var newElem = new StackElement<T>(obj) { _nextElement = this.top };
+            this.top = newElem;
+            ElemCount++;
+        }
+
+        public T Pop()
+        {
+            var popped = top;
+            top = top._nextElement;
+            ElemCount--;
+            return popped.data;
+        }
+
+        public T Peek() => top.data;
+
+        public void PrintStack()
+        {
+            var temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp._nextElement;
+            }
+        }
+
+        public void PurgeStack()
+        {
+            while (top != null) Pop();
+        }
+        
+
+    }
+
+
+}
