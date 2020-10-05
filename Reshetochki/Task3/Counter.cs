709b65e65f8825fd9665ca7fc329120ae1083c2a
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-10-05 11:02:56 +0300

    Task_3 / add ClassObjectsCounter

---
+++ Reshetochki/Task3/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Counter
+    {
+        public static int ObjectsCounter { get; private set; } = 0;
+
+        public Counter()
+        {
+            ObjectsCounter++;
+        }
+
+    }
+}
--- Reshetochki/Task3/Program.cs
+++ Reshetochki/Task3/Program.cs
@@ -10,10 +10,58 @@ namespace Task3
         private static SafeClass<string> _strSafe;
         private static SafeClass<char> _chrSafe;
         private static SafeClass<bool> _blSafe;
+
+
+        static void ClassObjectsCounter()
+        {
+            Console.WriteLine("If you want to create objects manually, write '1'.\n" +
+                "If you want to creat objects with random from 0 to 1000, write '2'\n" +
+                "If you want to know a total number of objects, write '3'.\n" +
+                "If you want to exit, write '4'");
+            var num = true;
+            while (num)
+            {
+                var selection = Console.ReadLine();
+                switch (selection)
+                {
+                    case "1":
+                        Console.Write("How many objects do you want to create?  : ");
+                        var objs = int.Parse(Console.ReadLine());
+                        for (var i = 0; i < objs; i++)
+                        {
+                            var NewObject = new Counter();
+                        }
+                        Console.WriteLine($"{objs} objects have been created. What's next?");
+                        break;
+                    case "2":
+                        var rnd = new Random();
+                        var value = rnd.Next(0, 1000);
+                        Console.WriteLine($"Random wants to create {value} objects");
+                        for (var i = 0; i < value; i++)
+                        {
+                            var newObject = new Counter();
+                        }
+                        Console.WriteLine($"{value} objects have been created. What's next?");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Summarily created: {Counter.ObjectsCounter} objects. What's next?");
+                        break;
+                    case "4":
+                        Console.WriteLine("Press Enter to exit");
+                        num = false;
+                        break;
+                    default:
+                        Console.WriteLine("Write again");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
         private static void SafeClass()
         {
-          
-            
+
+
             do
             {
                 Console.WriteLine("Create safe - 1.\n Show var in safe - 2.");
@@ -24,7 +72,7 @@ namespace Task3
                     case 1:
                         Console.WriteLine("1.INT.\n 2.BOOL.\n3.CHAR.\n4.STRING.\n5.DOUBLE");
                         var create = int.Parse(Console.ReadLine() ?? string.Empty);
-                        
+
                         switch (create)
                         {
                             case 1:
@@ -37,7 +85,7 @@ namespace Task3
                                 break;
                             case 3:
                                 Console.WriteLine("Enter char");
-                                _chrSafe = new SafeClass<char>(Console.ReadLine().First(x=>x is char));
+                                _chrSafe = new SafeClass<char>(Console.ReadLine().First(x => x is char));
                                 break;
                             case 4:
                                 Console.WriteLine("Enter string");
@@ -59,19 +107,19 @@ namespace Task3
                             {
                                 case 1:
                                     Console.WriteLine($"{_intSafe.GetValue()}");
-                                    
+
                                     break;
                                 case 2:
                                     Console.WriteLine($"{_blSafe.GetValue()}");
-                                    
+
                                     break;
                                 case 3:
                                     Console.WriteLine($"{_chrSafe.GetValue()}");
-                                   
+
                                     break;
                                 case 4:
                                     Console.WriteLine($"{_strSafe.GetValue()}");
-                                    
+
                                     break;
                                 case 5:
                                     Console.WriteLine($"{_dblSafe.GetValue()}");
@@ -83,15 +131,16 @@ namespace Task3
                         {
                             Console.WriteLine("YOu must create safe before reading it!!!!!!");
                             Console.WriteLine(ex.ToString());
-                            
+
                         }
+
                         break;
                 }
 
             } while (true);
 
-            var s2 = int.Parse(Console.ReadLine() ?? string.Empty);
         }
+
         static void Main(string[] args)
         {
             int select;
@@ -103,13 +152,13 @@ namespace Task3
                 switch (select)
                 {
                     case 1:
-                        //TODO ObjectCounter
+                        ClassObjectsCounter();
                         break;
                     case 2:
                         SafeClass();
                         break;
                     case 3:
-                        //TODO Polymorphism
+                        
                         break;
 
                 }
