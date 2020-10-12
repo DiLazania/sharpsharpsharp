3567892ef92ec41b51f6dd22b07df96239d7e1a6
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-10-12 04:15:58 +0300

    Task4_1 / added rectangle drawing in WPF with abstract class

---
+++ Reshetochki/Task4_AbstractDraw/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task4_AbstractDraw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Figure fig;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var rn = new Random();
+            switch (rn.Next(1, 4))
+            {
+                case 1: 
+                    fig = new Rect();
+                    gen.Content = "Generated";
+                    break;
+            }
+        }
+        
+        private void DrawBtn_Click(object sender, RoutedEventArgs e)
+        {
+            fig.Draw(canv);
+        }
+    }
+}
---
+++ Reshetochki/Task4_AbstractDraw/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace Task4_AbstractDraw
+{
+    class Rect:Figure
+    {
+        public int A { get; private set; }
+        public int B { get; private set; }
+
+        private Brush paintBrush;
+
+        public override void Draw(Canvas canvas)
+        {
+            var rn = new Random();
+            var rect = new System.Windows.Shapes.Rectangle();
+            rect.Stroke = paintBrush;
+            rect.Width = A;
+            rect.Height = B;
+            Canvas.SetLeft(rect,rn.Next(50, 700));
+            Canvas.SetTop(rect, rn.Next(50, 700));
+            canvas.Children.Add(rect);
+        }
+        public Rect()
+        {
+            var rn = new Random();
+            A = rn.Next(10, 100);
+            B = rn.Next(10, 100);
+            paintBrush = new SolidColorBrush( Color.FromRgb((byte)rn.Next(1,255), (byte)rn.Next(1,255), (byte)rn.Next(1,255))); 
+        }
+
+    }
+}
