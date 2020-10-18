6624b760614d1e591184f08c8087bf857425d701
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-10-18 22:25:25 +0300

    Task 4/ figures/ add new data input window

---
+++ Reshetochki/Task4_AbstractDraw/InputWnd.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task4_AbstractDraw
+{
+    /// <summary>
+    /// Interaction logic for InputWnd.xaml
+    /// </summary>
+    public partial class InputWnd : Window
+    {
+        public InputWnd()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
