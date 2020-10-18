25d048642b66053be156966ed00f726c24b4cd30
Anastasiya <chubaeva.nastya@mail.ru>
2020-10-18 22:58:05 +0300

    Task 5 with solution for creating library

---
+++ Reshetochki/SwitchLibrary/SwitchLib.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace Switch_Lib
+{
+    public class SwitchLib
+    {
+        public delegate void FunctionDelegat();
+        public void Switch(FunctionDelegat function1, FunctionDelegat function2, FunctionDelegat function3)
+        {
+            Console.WriteLine($"Choose 1: {function1.GetMethodInfo().Name} \n" +
+                              $"       2: {function2.GetMethodInfo().Name} \n" +
+                              $"       3: {function3.GetMethodInfo().Name}");
+            int select = int.Parse(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    function1();
+                    break;
+                case 2:
+                    function2();
+                    break;
+                case 3:
+                    function3();
+                    break;
+                default:
+                    Console.WriteLine("try again");
+                    break;
+            }
+        }
+    }
+}
---
+++ Reshetochki/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Switch_Lib;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchLib.FunctionDelegat function1, function2, function3;
+            function1 = Library;
+            function2 = Task2; //!!! <- delete comment after corecting
+            function3 = Task3; //!!! <- delete comment after corecting
+            SwitchLib f = new SwitchLib();
+            f.Switch(function1, function2, function3);
+            Console.ReadKey();
+        }
+        public static void Library()
+        {
+            Console.WriteLine("I just creat a library");
+        }
+
+        public static void Task2() //!!! <- delete comment after corecting
+        {
+
+        }
+        public static void Task3() //!!! <- delete comment after corecting
+        {
+
+        }
+    }
+}
