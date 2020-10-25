cdafefe11886b5cd09e558cdecf2f71cdbcd738f
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-10-26 00:49:28 +0300

    Task 5: refactored SwitchLib to allow multiple function choice.
    Task 6: added Hitler Age Calculator

--- Reshetochki/SwitchLibrary/SwitchLib.cs
+++ Reshetochki/SwitchLibrary/SwitchLib.cs
@@ -6,27 +6,14 @@ namespace Switch_Lib
     public class SwitchLib
     {
         public delegate void FunctionDelegat();
-        public void Switch(FunctionDelegat function1, FunctionDelegat function2, FunctionDelegat function3)
+        public void Switch(params FunctionDelegat[] functions)
         {
-            Console.WriteLine($"Choose 1: {function1.GetMethodInfo().Name} \n" +
-                              $"       2: {function2.GetMethodInfo().Name} \n" +
-                              $"       3: {function3.GetMethodInfo().Name}");
-            int select = int.Parse(Console.ReadLine());
-            switch (select)
+            var i = 0;
+            foreach (var func in functions)
             {
-                case 1:
-                    function1();
-                    break;
-                case 2:
-                    function2();
-                    break;
-                case 3:
-                    function3();
-                    break;
-                default:
-                    Console.WriteLine("try again");
-                    break;
+                Console.WriteLine($"Choose function to execute:\n {i++} : {func.GetMethodInfo()?.Name}");
             }
+            functions[int.Parse(Console.ReadLine()!)].Invoke();
         }
     }
 }
---
+++ Reshetochki/Task6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using Switch_Lib;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = new SwitchLib();
+            menu.Switch(HitlerTime);
+        }
+
+        public static void HitlerTime()
+        {
+            Console.InputEncoding = Encoding.ASCII;
+            var hitlerBirth = new DateTime(1889, 4, 20, 18, 30, 57,324);
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"Enter date in following format dd%MM^yyyy : ");
+                    var input = Console.ReadLine();
+                    var targetTime = DateTime.ParseExact(input, @"dd\%MM^yyyy", CultureInfo.InvariantCulture);
+                    var difference = DateTime.MinValue + (targetTime - hitlerBirth);
+                    Console.WriteLine(
+                        $"\nFrom Hitler's birth:\n {difference.Year - 1} Years\n {difference.Month - 1} Months\n" +
+                        $" {difference.Day - 1} Days\n {difference.Hour} Hours\n {difference.Minute} Minutes\n {difference.Second} " +
+                        $"Seconds\n {difference.Millisecond} Milliseconds\n {difference.Ticks} Ticks.");
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
