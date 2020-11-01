b305111c79b75123d0cded446ff6fdfda07fdeb5
s.poludnevich.x19 <sergey.polud19@gmail.com>
2020-11-01 18:42:45 +0300

    Task 7: Added Rainbow Colors Enumeration

---
+++ Reshetochki/Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using Switch_Lib;
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var menu = new SwitchLib();
+            menu.Switch(EnumerateRainbow);
+        }
+
+
+        public static void EnumerateRainbow()
+        {
+            var textColor = System.ConsoleColor.Red;
+            foreach (var color in Enum.GetValues(typeof(Rainbow.RainbowColors)))
+            {
+                Console.WriteLine(Enum.GetName(typeof(Rainbow.RainbowColors), color));
+                Console.ForegroundColor = textColor++;
+            }
+        }
+    }
+}
---
+++ Reshetochki/Task7/Rainbow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task7
+{
+    class Rainbow
+    {
+        public enum RainbowColors
+        {
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Cyan,
+            Blue,
+            Violet
+        }
+
+    }
+}
